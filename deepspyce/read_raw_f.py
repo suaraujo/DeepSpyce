f41697ad3043d6a79acb172cf9c3cfde61673002
Marcelo Colazo <mcolazo@gmail.com>
2020-11-17 06:39:02 -0300

    test data

--- deepspyce/read_raw_f.py
+++ deepspyce/read_raw_f.py
@@ -2,6 +2,7 @@ import os
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+import struct
 #import statistics as st
 
 #records = 0
@@ -43,6 +44,8 @@ def spprom(raw):
 def read_mdata(mdata):
     '''
     Leer archivos metadata con datos de la observacion
+    Datos a extraer:
+    nombre de la fuente, RA, DEC, Fecha obs, Hora obs UTC
     '''
     pass
 
@@ -52,12 +55,37 @@ def df2fb(dframe):
     '''
     pass
 
+def invrawch(raw):
+    '''
+    Invierte orden de canales para formato FB
+    '''
+    spch = 2048
+    bspl = 8
+    size = os.path.getsize(raw.name)
+    nrec = int(size/spch/bspl)
+    gf = open('test.dat','wb')
+    for rec in range(0,nrec):
+        spraw = raw.read(spch*bspl)
+        splis = struct.unpack_from(">2048q",spraw)
+        splis = list(splis)
+        splis.reverse()
+        splis = tuple(splis)
+        spinv = struct.pack(">2048q",*splis)
+        gf.write(spinv)
+    return 
+
+
 if __name__ == "__main__":
-    fd = open("../raw_data/20201027_133329_1m.raw", "rb")
+    fd = open("../raw_data/20201027_133329_test.raw", "rb")
+#    fd1 = open("./test.dat", "rb")
+#    invrawch(fd)
     data = raw2df(fd)
     print(data[0])
-    spprom(data)
+#    data2  = raw2df(fd1)
+#    print(data2[3])
+#    spprom(data2)
     fd.close()
+#    fd1.close()
 #    spprom = data.mean(axis=1)
 #    print(len(spprom))
 #    print(spprom[0])
